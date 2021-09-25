c238834d88b99e6881e88e1489144499c96f2971
Zait06 <zait-hdez@hotmail.com>
2021-09-25 18:58:42 -0500

    Strings and if-else

---
+++ 04_string/main.py
@@ -0,0 +1,21 @@
+# Strings
+
+frase = input("Ingrese una frase: ")
+
+print(len(frase))
+
+bien = ", yo estoy bien"
+
+# Concatenacion con strings
+resultado = frase + bien
+print(resultado)
+
+# Repeticion
+resultado = frase * 5
+print(resultado)
+
+# Todo en minusculas
+print(frase.lower())
+
+# Todo en mayusculas
+print(frase.upper())
--- 05_condiciones/main.py
+++ 05_condiciones/main.py
@@ -1,4 +1,4 @@
-pelotas = 3
+pelotas = int(input("Ingrese un numero de pelotas: "))
 
 if pelotas == 0:
     print("No hay pelotas")
@@ -6,3 +6,21 @@ elif pelotas>0 and pelotas <=2:
     print("Hay pelotas suficientes")
 else:
     print("Hay demasiadas pelotas")
+
+#########################################################################
+
+promedio = float(input("Ingresa tu promedio: "))
+'''
+    0 - 3.9999999 = eres un burro       [0,4)
+    4 - 7 = casi mueres       [4,7)
+    7 - 9 = puedes mejorar      [7,9)
+      > 9 = que matado eres     [9,inf)
+'''
+if promedio >= 0 and promedio < 4:
+    print("Eres un burro")
+elif promedio >= 4 and promedio < 7:
+    print("Casi mueres")
+elif promedio >= 7 and promedio < 9:
+    print("Puedes mejorar")
+else:
+    print("Que matado eres")
