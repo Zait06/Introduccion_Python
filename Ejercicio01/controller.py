0c99dd49afe66bf2e663bd51c8368cc6186edef9
Zait06 <zait-hdez@hotmail.com>
2021-11-05 19:00:03 -0600

    Modulos

--- Ejercicio01/controller.py
+++ Ejercicio01/controller.py
@@ -9,6 +9,8 @@
 
 
 from dataJSON import data
+# from dataJSON import suma as adicion
+import random as rand
 
 def deleteNotif(index):
     data.pop(index-1)
@@ -16,6 +18,11 @@ def deleteNotif(index):
 def deleteLast():
     data.pop()
 
+def deleteFirst():
+    # data.pop(0)
+    deleteNotif(0)
+
+# Tamanio de los datos
 def numNotif():
     return len(data)
 
@@ -24,3 +31,30 @@ def readNotif(index):
 
 def readAll():
     return data
+
+def readLast():
+    return data[-1]
+
+def creatNotif():
+    title = input('Ingrese el titulo: ')
+    body =  input('Ingrese el cuerpo de la notificacion: ')
+    userId = rand.randint(1, 10)
+    idNotif = rand.randint(1, 1000)
+    notification = {
+        "userId": userId,
+        "id": idNotif,
+        "title": title,
+        "body": body 
+    }
+    data.append(notification)
+
+def editNotif(index):
+    notif = readNotif(index)
+    title = input(f"Ingrese el titulo [{notif['title']}]: ")
+    body =  input('Ingrese el cuerpo de la notificacion [{}]: '.format(notif['body']))
+    if title != '':
+        notif['title'] = title
+    if body != '':
+        notif['body'] = body
+    data[index] = notif
+    
--- Ejercicio01/main.py
+++ Ejercicio01/main.py
@@ -4,8 +4,19 @@ import controller
 
 print("Programa principal")
 
+
+# print('{} {} {}'.format(True, 34, 'hola'))
+
 print(controller.numNotif())
 print(controller.readNotif(5))
 controller.deleteNotif(3)
 print(controller.numNotif())
+print(controller.readNotif(5))
+
+# print(f"Antes: {controller.numNotif()}")
+# controller.creatNotif()
+# print(f"Despues: {controller.numNotif()}")
+# print(controller.readLast())
+
+controller.editNotif(5)
 print(controller.readNotif(5))
